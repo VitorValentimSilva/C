4c3cfcbcdc21dc72b360d6b3639feddf142a0dc3
Vitor Valentim <vitorvalentin840@gmail.com>
2023-05-03 18:26:07 -0300

    aa

---
+++ Exercicio30.cpp
@@ -0,0 +1,74 @@
+/*
+Faça um programa que receba o preço, a categoria (1 - limpeza, 2 - alimentação, 3 - vestuário) e a situação 
+(R - produtos que necessitam de refrigeração e N - produtos que não necessitam de refrigeração). Calcule e exiba:
+
+a)	O valor do aumento, usando as regras que seguem:
+
+PREÇO	CATEGORIA	PERCENTUAL DE AUMENTO
+<= 25	1			5%
+		2			8%
+		3			10%
+> 25	1			12%
+		2			15%
+		3			18%
+
+b)	O valor do imposto, usando as regras que seguem:
+
+O produto que preencher pelo menos um dos seguintes requisitos pagará imposto equivalente a 5% do preço; caso contrário,
+pagará 8%. Os requisitos são:
+
+Categoria : 2
+Situação : R
+
+c)	O novo preço, ou seja, o preço mais o aumento menos o imposto.
+d)	A classificação, usando as regras a seguir:
+
+NOVO PREÇO				CLASSIFICAÇÃO
+<= 50,00				Promoção
+Entre 50,00 e 120,00	Normal
+>= 120,00				Extra
+*/
+
+#include <stdio.h>
+
+int main(){
+	
+	
+	
+	printf("Preco: \n");
+	scanf("%f", &pre);
+	
+	printf("Categoria: \n");
+	printf("Limpeza - [1] \n");
+	printf("Alimentacao - [2] \n");
+	pritnf("Vestuario - [3] \n");
+	scanf("%d", &cate);
+	
+	printf("Situacao: \n");
+	printf("Produtos que necessitam de refrigeracao - [r] \n");
+	printf("Produtos que não necessitam de refrigeracao - [n] \n");
+	scanf("%s", &situ);
+	
+	if (pre <= 25){
+		if (cate == 1){
+			npre = pre * 105 / 100;
+		}
+		else if (cate == 2){
+			npre = pre * 108 / 100;
+		}
+		else{
+			npre = pre * 110 / 100;
+		}	
+	}
+	else{
+		if (cate == 1){
+			npre = pre * 112 / 100;
+		}
+		else if (cate == 2){
+			npre = pre * 115 / 100;
+		}
+		else{
+			npre = pre * 118 / 100;
+		}
+	}
+}
