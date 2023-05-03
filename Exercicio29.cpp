9921fce22037979c42d40840ca0191fbca003f12
Vitor Valentim <vitorvalentin840@gmail.com>
2023-05-03 12:04:41 -0300

    n

--- Exercicio29.cpp
+++ Exercicio29.cpp
@@ -17,41 +17,64 @@ i) quantas mulheres de até 20 anos disseram não.
 
 int main(){
 	
-	int i = 1;
-	char s[1], r[1];
+	int i = 1, ida, e = 1, h20 = 0, m20 = 0, ts = 0, ti = 0, tn = 0, ms, hn;
+	float s1, s2;
+	char s[2], r[2];
 	
-	do{
+	while(i != 0){
 		
 		printf("Sexo (m/f): \n");
-		scanf ("%s", &s);
+		scanf ("%s", &s[2]);
 		
 		printf("Idade: \n");
 		scanf("%d", &ida);
 		
 		printf("Gostou do novo produto: \n");
 		printf("Sim(s), Nao(n), Indiferente(i) \n");
-		scanf("%s", &r);
+		scanf("%s", &r[2]); 
 		
 		printf("Para sair digite (0): \n");
-		scanf("%d", &i); 
+		scanf("%d", &i);
 		
 		if (i != 0){
 			e++;
 		}
 		
-		if (r[1] == 's'){
+		if (r[2] == 's'){
 			ts++;
+			if (s[2] == 'f'){
+				ms++;
+			}
+			else if (s[2] == 'm' && i <= 20){
+				h20++;
+			}
 		}
 		
-		if (r[1] == 'n'){
+		if (r[2] == 'n'){
 			tn++;
+			if (s[2] == 'm'){
+				hn++;
+			}
+			else if (s[2] == 'f' && i <= 20){
+				m20++;
+			}
 		}
 		
-		if (r[1] == 'i'){
+		if (r[2] == 'i'){
 			ti++;
-		}
-		
-		
-		
-	} while(i != 0)
+		}	
+	}
+	
+	s1 = ts + tn;
+	s2 = tn * 100 / s1;
+	
+	printf("Quantidade de pessoas que foram entrevistadas: %d \n",e);
+	printf("Quantidade de pessoas que disseram sim: %d \n",ts);
+	printf("Quantidade de pessoas que disseram nao: %d \n",tn);
+	printf("Quantidade de pessoas que disseram indiferente: %d \n",ti);
+	printf("Percentual de pessoas que disseram nao: %.2f%% \n",s2);
+	printf("Quantidade de mulheres que disseram sim: %d \n",ms);
+	printf("Quantidade de homens que disseram nao: %d \n",hn);
+	printf("Quantidade de homens de ate 20 anos disseram sim: %d \n",h20);
+	printf("Quantidade de mulhres de ate 20 anos disseram nao: %d",m20);
 }
